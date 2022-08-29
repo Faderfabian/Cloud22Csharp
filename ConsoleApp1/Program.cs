9cf747e54b93a6e17a6fb697df77fe5089b94f18
Fabbe <Fabbe@DESKTOP-PVHEDAK>
2022-08-29 14:17:43 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+var tala = 5;
+var talb = 6;
+
+Console.Write("Skriv ditt namn:");
+    string name = Console.ReadLine();
+
+if (name == "fabbe")
+{
+    Console.Write("Hej Fabbe!");
+
+}
+else if (name == "marcus")
+{
+    
+    Console.WriteLine("Hejsan Marcus!");
+}
+else
+{
+    Console.WriteLine(tala + talb);
+}*/
+/*
+int i = 1;
+while (i <10)
+
+{
+    Console.Write(i);
+    i = i + 1;
+}*/
+/*
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+*/
+/*
+int i = 10;
+
+while (i > 0)
+{
+    Console.WriteLine(i);
+    i--;
+
+}
+*/
+
+/*
+Console.Write("skriv ett namn:");
+string name = Console.ReadLine();
+
+for (int i = 1; i < 10; i++)
+
+Console.WriteLine("Hej " + name);*/
+
+/*
+Console.Write("skriv ditt namn: ");
+string name = Console.ReadLine();
+int i = 1;
+while (i < 10)
+{
+    Console.WriteLine(i + "." + name);
+    i++;
+}
+*/
+/*
+var math1 = (-1 + 4 * 6); 
+var math2 = ((35 + 5) % 7);
+var math3 = (14 + -4 * 6 / 11);
+var math4 = (2 + 15 / 6 * 1 - 7 % 2);
+Console.WriteLine(math1);
+Console.WriteLine(math2);
+Console.WriteLine(math3); 
+Console.WriteLine(math4);
+
+*/
+
+var math1 = 1;
+math1 = math1 * 2;
+Console.WriteLine("Enter number to be multiplied by 2:");
+Console.WriteLine("your answer is:" + math1);
+
+
+
+
+
